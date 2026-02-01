302c0a9576f8700df9a5b0ab3bb28b442bc17b68
cyy-07 <3141925174a@gmail.com>
2026-02-01 00:49:22 -0800

    Day 2.2: add some changes on puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -10,3 +10,28 @@ class Puzzle:
                 if self.state[i][j] == 0:
                     return (i, j)
         return None
+    
+    def copy(self):
+        # We want to create a copy of the current puzzle state, to better compare states.
+        state_copy = []
+        for row in self.state:
+            state_copy.append(row.copy())
+        return state_copy
+    
+
+
+    def check_is_goal(self):
+        # We want to check if the current state is the same with the goal state.
+         goal = [[1,2,3],
+                [4,5,6],
+                [7,8,0]]
+         return self.state == goal
+        # Try to display the puzzle 
+    def display(self):
+        for row in self.state:
+            for num in row:
+                if num == 0:
+                    print("*", end=" ")
+                else:
+                    print(num, end=" ")
+            print()
